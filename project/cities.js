e1ba7ad27779f445bf48b4d632acf04a993a5f19
loopera <arianneaquino24@gmail.com>
2023-10-18 01:45:00 +0100

    project working but needs editing

---
+++ project/cities.js
@@ -0,0 +1,6 @@
+// Define and export the 'cities' array
+
+
+const cities = ['New York', 'London', 'Paris', 'Manila', 'Panabo City', 'Tokyo', 'Salt Lake City', 'Davao City', 'Berlin'];
+
+export default cities;
--- project/script.js
+++ project/script.js
@@ -1,5 +1,4 @@
-// Import the getWeatherRecommendation function from the weatherRecommendations module
-import { getWeatherRecommendation } from './weatherRecommendations.js';
+import cities from "./cities.js";
 
 // Replace with your own OpenWeatherMap API key
 const apiKey = '7115d3e4427a2e9c252b2e76619572c2';
@@ -8,8 +7,10 @@ const citySelect = document.getElementById('citySelect');
 const getWeatherButton = document.getElementById('getWeatherButton');
 const weatherInfo = document.getElementById('weatherInfo');
 
+
+
 // Array of cities
-const cities = ['New York', 'London', 'Paris', 'Manila', 'Panabo City', 'Tokyo', 'Salt Lake City', 'Davao City', 'Berlin'];
+// const cities = ['New York', 'London', 'Paris', 'Manila', 'Panabo City', 'Tokyo', 'Salt Lake City', 'Davao City', 'Berlin'];
 
 // Use the map method to create an array of option elements for the city dropdown
 const cityOptions = cities.map((city) => {
@@ -36,7 +37,6 @@ getWeatherButton.addEventListener('click', () => {
   }
 });
 
-
 // Function to fetch weather data from OpenWeatherMap
 function getWeather(city) {
   // Replace 'YOUR_OPENWEATHERMAP_API_KEY' with your actual API key
@@ -48,8 +48,18 @@ function getWeather(city) {
       const temperature = data.main.temp - 273.15; // Convert to Celsius
       const description = data.weather[0].description;
 
-      // Use the imported function to get weather recommendations
-      const recommendation = getWeatherRecommendation(description);
+      let recommendation = '';
+
+      // Perform conditional branching based on weather conditions
+      if (description.includes('rain')) {
+        recommendation = 'Don\'t forget your umbrella!';
+      } else if (description.includes('clouds')) {
+        recommendation = 'It might be cloudy today.';
+      } else if (description.includes('clear')) {
+        recommendation = 'Enjoy the clear skies!';
+      } else {
+        recommendation = 'Have nice day.';
+      }
 
       const weatherText = `Weather in ${city}: <br>${description} <br><br>Temperature: ${temperature.toFixed(2)}°C. <br><br>${recommendation}`;
       weatherInfo.innerHTML = weatherText;

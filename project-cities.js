1e6d40fb0bc8033cfa31d0bc84a44f9a9fded180
loopera <arianneaquino24@gmail.com>
2023-10-19 13:17:45 +0100

    project commit

--- project-cities.js
+++ project-cities.js
@@ -1,5 +1,4 @@
-// Define and export the 'cities' array
-
+// Define and export the 'cities' array and then Export Module
 
 const cities = ['New York', 'London', 'Paris', 'Manila', 'Panabo City', 'Tokyo', 'Salt Lake City', 'Davao City', 'Berlin'];
 
--- project-script.js
+++ project-script.js
@@ -1,16 +1,17 @@
+// IMporting ES  Module
 import cities from "./project-cities.js";
 
-// Replace with your own OpenWeatherMap API key
+// Declaring Variables
 const apiKey = '7115d3e4427a2e9c252b2e76619572c2';
-
 const citySelect = document.getElementById('citySelect');
 const getWeatherButton = document.getElementById('getWeatherButton');
 const weatherInfo = document.getElementById('weatherInfo');
 
 
 
-// Array of cities
+// Array of cities - Transferred into a Module
 // const cities = ['New York', 'London', 'Paris', 'Manila', 'Panabo City', 'Tokyo', 'Salt Lake City', 'Davao City', 'Berlin'];
+
 
 // Use the map method to create an array of option elements for the city dropdown
 const cityOptions = cities.map((city) => {
@@ -37,9 +38,11 @@ getWeatherButton.addEventListener('click', () => {
   }
 });
 
-// Function to fetch weather data from OpenWeatherMap
+
+
+// Function to fetch weather data from OpenWeatherMap. Using Fetch()
+
 function getWeather(city) {
-  // Replace 'YOUR_OPENWEATHERMAP_API_KEY' with your actual API key
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
   fetch(apiUrl)
@@ -58,14 +61,10 @@ function getWeather(city) {
       } else if (description.includes('clear')) {
         recommendation = 'Enjoy the clear skies!';
       } else {
-        recommendation = 'Have nice day.';
+        recommendation = 'Stay warm.';
       }
 
       const weatherText = `Weather in ${city}: <br>${description} <br><br>Temperature: ${temperature.toFixed(2)}°C. <br><br>${recommendation}`;
       weatherInfo.innerHTML = weatherText;
     })
-    .catch((error) => {
-      weatherInfo.textContent = 'An error occurred while fetching weather data.';
-      console.error(error);
-    });
 }
